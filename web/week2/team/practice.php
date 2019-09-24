76bda44644f96977e8462ebec7b8748a60e177f2
Keaton Sant <san16072@byui.edu>
2019-09-24 15:52:48 -0600

    prectice team activity

---
+++ web/week2/team/practice.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        for ($i = 0; $i < 10; $i++) {
+            if ($i%2) {
+                echo "<div id=$i style="color:blue">I am Blue and div $i</div>";
+
+            } else 
+                echo "<div id=$i style="color:red">I am Red and div $i</div>";
+        }
+    ?>
+</body>
+</html>
