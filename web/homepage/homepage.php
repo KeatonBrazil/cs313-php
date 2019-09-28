803dac631c2fff9237a4eae99e604032a1f6bd67
Keaton Sant <san16072@byui.edu>
2019-09-28 17:41:28 -0600

    adding shark png

--- web/homepage/homepage.php
+++ web/homepage/homepage.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="shark.png" type="image/gif">
     <title>Welcome</title>
 </head>
 <body>
@@ -14,3 +15,4 @@
     </header>
 </body>
 </html>
+
