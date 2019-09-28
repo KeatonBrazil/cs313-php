6fa45f31d3877c9b0d9ecc9cb79b109867fd7ae1
Keaton Sant <san16072@byui.edu>
2019-09-27 23:07:29 -0600

    Pushing my team assignment and the start of my homepage

---
+++ web/week2/team/ta2.js
@@ -0,0 +1,9 @@
+function clickedy () {
+    alert("Freak!!!!!!");
+}
+
+function changeColor () {
+    mytext = document.getElementById("color").value;
+    document.getElementById("a").style.color = mytext;
+
+}
