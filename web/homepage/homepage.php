df828a7b3dc00a9f556eb4fd67fcd7236d38f3b1
Keaton Sant <san16072@byui.edu>
2019-09-28 17:45:48 -0600

    adding shark png

--- web/homepage/homepage.php
+++ web/homepage/homepage.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="shark.png" type="image/gif">
-    <title>Epic Sharks</title>
+    <title>Epic Sharks | Welcome</title>
 </head>
 <body>
     <header>
