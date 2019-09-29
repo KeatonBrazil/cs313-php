f2068023f3fa752b2649f3aa978520211cd44dfc
Keaton Sant <san16072@byui.edu>
2019-09-28 23:23:20 -0600

    javascript

--- web/homepage/homepage.js
+++ web/homepage/homepage.js
@@ -1,7 +1,3 @@
-function changePic () {
-    doc = document.getElementById("us").src;
-    picUs = document.getElementById("us").src = "us.jpg";
-    picBaby = document.getElementById("us").src = "ultrasound.jpg";
-    
-    console.log(doc);
+function changePic () {    
+    console.log(document.getElementById("us").src.value);
 }
