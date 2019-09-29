663f3829e7b048c5133f1725a627a1b8a37d102b
Keaton Sant <san16072@byui.edu>
2019-09-29 00:56:30 -0600

    php

--- web/homepage/aboutme.php
+++ web/homepage/aboutme.php
@@ -28,11 +28,27 @@
         <div class = "item_right">
             <div>
                 <p id="pbaby">
-                    My wife and I car having a baby!
+                    My wife and I have been married for nearly 2 years now. 
+                    We met during our first semester of college and ever 
+                    since we started dating we knew we were meant to be together.
+                    We are so excited for the arrival of our new baby girl in a 
+                    few short months! (Click on photo)
+                </p>
+                    I have been studying at Brigham Young University of Idaho to 
+                    earn my Bachelor's Degree of Science in Data Science. I am 
+                    currently looking for internshios as a data scientist.
+                <p>
+
                 </p>
             </div>
         </div>
     </div>
+    <footer>
+        <p>
+            This page was designed by Keaton Sant<br>
+        </p>
+        <p><b>&copy; 2019 Keaton Sant<b></p>
+    </footer>
 
 </body>
 </html>
