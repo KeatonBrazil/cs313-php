168a687af2dfd6bcca8e3ea4cddd66102e389ff2
Keaton Sant <san16072@byui.edu>
2019-09-28 18:02:46 -0600

    change background color

--- web/homepage/homepage.php
+++ web/homepage/homepage.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="shark.png" type="image/gif">
+    <link rel="stylesheet" type="text/css" href="homepage.css">
     <title>Epic Sharks | Welcome</title>
 </head>
 <body>
     <header>
         <div>
-            <h1>Introduction</h1>
+            <h1>Welcome</h1>
         </div>
     </header>
 </body>
