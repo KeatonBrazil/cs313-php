17885e611ee0fd2fa536260c31b52328b399bbf4
Keaton Sant <san16072@byui.edu>
2019-09-28 20:47:14 -0600

    edit photo

--- web/homepage/homepage.php
+++ web/homepage/homepage.php
@@ -26,8 +26,11 @@
         <a class="link" href="assignments.php">Assignments</a>        
     </div>
     <div class = "container">
-        <div class = "itemL">
+        <div class = "item_left">
             <img id="me" src="me.jpg" alt="A professional photograph of Keaton Sant">
+        </div>
+        <div class = "item_right">
+
         </div>
     </div>
 </body>
