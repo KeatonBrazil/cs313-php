07adc57867980b354547489dd8ced8df0c82d028
Keaton Sant <san16072@byui.edu>
2019-09-28 22:25:46 -0600

    added an about me page and assignments page

---
+++ web/homepage/aboutme.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="shark.png" type="image/gif">
+    <link rel="stylesheet" type="text/css" href="homepage.css">
+    <title>Epic Sharks | About Me</title>
+</head>
+<body>
+    <header>
+        <h1>About Me</h1>
+    </header>
+    <div class="navbar sticky">
+        <a class="link" href="homepage.php">About Me</a>  
+        <a class="link" href="assignments.php">Welcome</a>              
+    </div>
+</body>
+</html>
---
+++ web/homepage/assignments.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="shark.png" type="image/gif">
+    <link rel="stylesheet" type="text/css" href="homepage.css">
+    <title>Epic Sharks | Assignments</title>
+</head>
+<body>
+    <header>
+        <h1>Assignments</h1>
+    </header>
+    <div class="navbar sticky">
+        <a class="link" href="aboutme.php">About Me</a>  
+        <a class="link" href="homepage.php">Welcome</a>              
+    </div>
+</body>
+</html>
