5c063fbb1c7a0f13e3fc110a2a862a88dd0ea80d
Keaton Sant <san16072@byui.edu>
2019-09-28 23:55:24 -0600

    javascript

--- web/homepage/homepage.js
+++ web/homepage/homepage.js
@@ -2,7 +2,8 @@ function changePic () {
     if (document.getElementById("us").src == "us.jpg") {
     document.getElementById("us").src = "her.JPG";
     console.log(document.getElementById("us").src);
-    } else if (document.getElementById("us").src == "her.JPG") {
+    } 
+    if (document.getElementById("us").src == "her.JPG") {
         document.getElementById("us").src = "us.jpg";
     } console.log(document.getElementById("us").src);
 }
