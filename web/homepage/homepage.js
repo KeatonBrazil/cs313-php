1635be347b53536fcdb98ea8f140df466ba95ed9
Keaton Sant <san16072@byui.edu>
2019-09-28 23:38:55 -0600

    javascript

--- web/homepage/homepage.js
+++ web/homepage/homepage.js
@@ -2,6 +2,6 @@ function changePic () {
     if (document.getElementById("us").src = "us.jpg") {
     document.getElementById("us").src = "her.JPG";
     } else {
-        document.getElementById("us").src = "us.jpg"
+        document.getElementById("us").src = "us.jpg";
     }
 }
