ad3abe4b54ce5da5f29582920d55afa0e5b099ea
Keaton Sant <san16072@byui.edu>
2019-10-02 19:57:44 -0600

    team project

--- web/week3/team/ta3.php
+++ web/week3/team/ta3.php
@@ -9,18 +9,18 @@
     for ($x=0; $x < $count; $x++) {
 
         if ($continent[$x] == "na") {
-            echo "North America<br>";
-        } if else ($continent[$x] == "sa") {
-            echo "South America<br>";
-        } if else ($continent[$x] == "eu") {
-            echo "Europe<br>";
-        } if else ($continent[$x] == "as") {
-            echo "Asia<br>";
-        } if else ($continent[$x] == "aus") {
-            echo "Australia<br>";
-        } if else ($continent[$x] == "af") {
-            echo "Africa<br>";
-        } else echo "Antarctica<br>";
+            echo "North America<br>"; }
+        // } if else ($continent[$x] == "sa") {
+        //     echo "South America<br>";
+        // } if else ($continent[$x] == "eu") {
+        //     echo "Europe<br>";
+        // } if else ($continent[$x] == "as") {
+        //     echo "Asia<br>";
+        // } if else ($continent[$x] == "aus") {
+        //     echo "Australia<br>";
+        // } if else ($continent[$x] == "af") {
+        //     echo "Africa<br>";
+        // } else echo "Antarctica<br>";
 
     }
 
