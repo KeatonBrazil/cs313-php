f8fe366f1ca8c262f14593c87db9d600a3a8eb75
Keaton Sant <san16072@byui.edu>
2019-10-05 19:22:08 -0600

    created for loop

--- web/shoppingcart/browse.php
+++ web/shoppingcart/browse.php
@@ -19,6 +19,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="shark.png" type="image/gif">
+    <link rel="stylesheet" type="text/css" media="screen" href="shopping.css" />
     <title>Epic Sharks | Browse</title>
 </head>
 <body>
@@ -32,7 +33,14 @@
         $gamePng = array('mysterium.png', 'rollGalaxy.png', 'scythe.png', 'terraformingMars.png');
         $count = count($gamePng);
         for ($x=0; $x < $count; $x++) {
-            echo "<div><form name='form" . $x . "' action='' method='post'> Helllo </form></div>";
+            echo "<div class='maindiv'><form name='form" . $x . "' action='' method='post'>";
+            echo "<div class='container'>";
+            echo "<div class='item1'>";
+            echo "<strong>" . $gameName[$x] . "</strong><br>";
+            echo "<p>Price: $" . $gamePrice[$x] . "</p></div>";
+            echo "<div class='item2'>";
+            echo "<img class='img' src='" . $gamePng[$x] . "' alt='Picture of the boardgame " . $gameName[$x] . "'></div>";
+            echo "</form></div>";
         }
     ?>
 
--- web/shoppingcart/login.php
+++ web/shoppingcart/login.php
@@ -10,6 +10,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="shark.png" type="image/gif">
+    <link rel="stylesheet" type="text/css" media="screen" href="shopping.css" />
     <title>Epic Sharks | Login</title>
 </head>
 <body>
