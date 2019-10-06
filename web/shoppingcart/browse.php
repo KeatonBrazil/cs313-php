492ededed69b939ab5f6b00b7f4250cfc1f5f355
Keaton Sant <san16072@byui.edu>
2019-10-05 22:08:44 -0600

    edited css

--- web/shoppingcart/browse.php
+++ web/shoppingcart/browse.php
@@ -37,14 +37,13 @@
 
         for ($x=0; $x < $count; $x++) {
             echo "<form name='form" . $x . "' action='' method='post'>";
-            echo "<div class='container" . $x . "'>";
+            echo "<div class='container'>";
             echo "<div class='item1'>";
             echo "<strong>" . $gameName[$x] . "</strong><br>";
             echo "<p>Price: $" . $gamePrice[$x] . "</p></div>";
             echo "<div class='item2'>";
             echo "<img class='img' src='" . $gamePng[$x] . "' alt='Picture of the boardgame " . $gameName[$x] . "'></div>";
             echo "</form>";
-            echo "<br><br><br>";
         }
 
         echo "</div>";
