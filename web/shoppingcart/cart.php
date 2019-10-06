dd81b421b3e20bdaa70b7dc9cf2aab8655216301
Keaton Sant <san16072@byui.edu>
2019-10-05 22:39:04 -0600

    edited css

---
+++ web/shoppingcart/cart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="shark.png" type="image/gif">
+    <link rel="stylesheet" type="text/css" media="screen" href="shopping.css" />
+    <title>Epic Sharks | Cart</title>
+</head>
+<body>
+    <header>
+        <?php echo $_SESSION['username'] . "'s Cart"; ?>
+    </header>
+    
+</body>
+</html>
