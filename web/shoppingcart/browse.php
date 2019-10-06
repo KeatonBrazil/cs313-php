aa5b6804e6cd697ba95d45f7cad0110372574183
Keaton Sant <san16072@byui.edu>
2019-10-05 23:08:12 -0600

    edited css

--- web/shoppingcart/browse.php
+++ web/shoppingcart/browse.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    if (isset($_SESSION['username'])) {
+    if (!isset($_SESSION['username'])) {
         $_SESSION['username'] = htmlspecialchars($_POST["user"]);
         if ($_SESSION['username'] == '') {
             header("Location: login.php");
