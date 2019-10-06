70ab6df33481d443ab42933d6486bd1996ae897e
Keaton Sant <san16072@byui.edu>
2019-10-05 22:40:22 -0600

    edited css

--- web/shoppingcart/cart.php
+++ web/shoppingcart/cart.php
@@ -1,3 +1,15 @@
+<?php 
+    session_start();
+
+    if (!isset($_SESSION['username'])) {
+        $_SESSION['username'] = htmlspecialchars($_POST["user"]);
+        if ($_SESSION['username'] == '') {
+            header("Location: login.php");
+            die();
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
