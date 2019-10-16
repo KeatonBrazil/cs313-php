61b0e8a8a54e090125d346fa8d870b3ebf5931e0
Keaton Sant <san16072@byui.edu>
2019-10-15 18:05:10 -0600

    css navbar

--- web/games/login.php
+++ web/games/login.php
@@ -4,20 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="games.css">
     <title>Shark Games | Login</title>
 </head>
 <body>
     <header><h1>Login</h1></header>   
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <ul class="nav navbar-nav">
-            <li class="active"><a href="login.php">Login</a></li>
-            <li><a href="signUp.php">Signup</a></li>
-            </ul>
-        </div>
-    </nav>        
+        <ul class="nav">
+            <li><a href="login.php">Login</a></li>
+            <li><a href="signUp.php">Sign up</a></li>
+        </ul>
+        
+           
 </body>
 </html>
 
