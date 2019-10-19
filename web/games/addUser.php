fe5940412f16896b66c6bc91ab2aaf07c6d6a508
Keaton Sant <san16072@byui.edu>
2019-10-19 17:15:58 -0600

    add account

--- web/games/addUser.php
+++ web/games/addUser.php
@@ -24,48 +24,50 @@
         die();
     }
 
-    $hashedPassword = password_hash($pass, PASSWORD_DEFAULT);
+    echo "good!"
 
-    require_once("gamesDb.php");
-    $db = get_db();
+    // $hashedPassword = password_hash($pass, PASSWORD_DEFAULT);
 
-    $query = 'SELECT username FROM game.member';
-    $stmt = $db -> prepare($query);
-    $stmt -> execute();
-    $names = $stmt -> fetchALL(PDO::FETCH_ASSOC);
+    // require_once("gamesDb.php");
+    // $db = get_db();
 
-    foreach ($names as $name) {
-        $old_name = $name['username'];
-        if ($user === $old_name) {
-            header("location: signUp.php");
-            die();
-        }
-    }
+    // $query = 'SELECT username FROM game.member';
+    // $stmt = $db -> prepare($query);
+    // $stmt -> execute();
+    // $names = $stmt -> fetchALL(PDO::FETCH_ASSOC);
 
-    $user_query = 'SELECT email FROM game.member';
-    $statement = $db->prepare($user_query);
-    $statement->execute();
-    $emails = $statement->fetchAll(PDO::FETCH_ASSOC);
+    // foreach ($names as $name) {
+    //     $old_name = $name['username'];
+    //     if ($user === $old_name) {
+    //         header("location: signUp.php");
+    //         die();
+    //     }
+    // }
+
+    // $user_query = 'SELECT email FROM game.member';
+    // $statement = $db->prepare($user_query);
+    // $statement->execute();
+    // $emails = $statement->fetchAll(PDO::FETCH_ASSOC);
     
-    foreach ($email as $mail) {
-        $old_email = $mail['email'];
-        if ($email === $old_email) {
-            header("Location: signUp.php");
-            die();
-        }
+    // foreach ($emails as $mail) {
+    //     $old_email = $mail['email'];
+    //     if ($email === $old_email) {
+    //         header("Location: signUp.php");
+    //         die();
+    //     }
     
-    $query = 'INSERT INTO game.member (username, pass_word, first_name, last_name, email) VALUES (:user, :pass, :fname, :lname, :email)';
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(':user', $user, PDO::PARAM_STR);
-    $stmt->bindValue(':pass', $hashedPassword, PDO::PARAM_STR);
-    $stmt->bindValue(':fname', $fname, PDO::PARAM_STR);
-    $stmt->bindValue(':lname', $lname, PDO::PARAM_STR);
-    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-    $result = $stmt->execute();
+    // $query = 'INSERT INTO game.member (username, pass_word, first_name, last_name, email) VALUES (:user, :pass, :fname, :lname, :email)';
+    // $stmt = $db->prepare($query);
+    // $stmt->bindValue(':user', $user, PDO::PARAM_STR);
+    // $stmt->bindValue(':pass', $hashedPassword, PDO::PARAM_STR);
+    // $stmt->bindValue(':fname', $fname, PDO::PARAM_STR);
+    // $stmt->bindValue(':lname', $lname, PDO::PARAM_STR);
+    // $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+    // $result = $stmt->execute();
 
-    flush();
-    header("Location:login.php");
-    die();
+    // flush();
+    // header("Location:login.php");
+    // die();
 
 
 
