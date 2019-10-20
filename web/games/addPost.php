0c5a11e1fa106196edf7336f08956d1af5b226a1
Keaton Sant <san16072@byui.edu>
2019-10-19 23:05:24 -0600

    add post

--- web/games/addPost.php
+++ web/games/addPost.php
@@ -31,7 +31,7 @@
     $query = 'INSERT INTO game.post (comment, time_date, member_id) VALUES (:comment, CURRENT_TIMESTAMP, :mem_id)';
     $stmt = $db ->prepare($query);
     $stmt->bindValue(':comment', $com, PDO::PARAM_STR);
-    $stmt->bindValue(':mem', $mem_id, PDO::PARAM_STR);
+    $stmt->bindValue(':mem_id', $mem_id, PDO::PARAM_STR);
     $newcomment = $stmt->execute();
 
     flush();
