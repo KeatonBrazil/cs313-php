e72b58b6b55eb917a9d923da4399310e14908c29
Keaton Sant <san16072@byui.edu>
2019-10-19 23:12:39 -0600

    add post

--- web/games/addPost.php
+++ web/games/addPost.php
@@ -14,35 +14,30 @@
     require_once("gamesDb.php");
     $db = get_db();
 
-    $com = $_POST['tarea'];
+    $com = $_POST['tarea'];    
 
-    echo $com;
-    
-
-    // $query = 'SELECT member_id, username FROM member';
-    // $stmt = $db->prepare($query);
-    // $stmt->execute();
-    // $user_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $query = 'SELECT member_id, username FROM member';
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $user_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     
 
-    // $mem_id = 0;
-    // foreach ($user_id as $member) {
-    //     if ($member['username'] === $username) {
-    //         $mem_id = $member['member_id'];
-    //     }
-    // }
+    $mem_id = 0;
+    foreach ($user_id as $member) {
+        if ($member['username'] === $username) {
+            $mem_id = $member['member_id'];
+        }
+    }
 
-    // echo $mem_id;
+    $query = 'INSERT INTO game.post (comment, time_date, member_id) VALUES (:comment, CURRENT_TIMESTAMP, :mem_id)';
+    $stmt = $db ->prepare($query);
+    $stmt->bindValue(':comment', $com, PDO::PARAM_STR);
+    $stmt->bindValue(':mem_id', $mem_id, PDO::PARAM_STR);
+    $newcomment = $stmt->execute();
 
-    // $query = 'INSERT INTO game.post (comment, time_date, member_id) VALUES (:comment, CURRENT_TIMESTAMP, :mem_id)';
-    // $stmt = $db ->prepare($query);
-    // $stmt->bindValue(':comment', $com, PDO::PARAM_STR);
-    // $stmt->bindValue(':mem_id', $mem_id, PDO::PARAM_STR);
-    // $newcomment = $stmt->execute();
-
-    // flush();
-    // header("Location:main.php");
-    // die();
+    flush();
+    header("Location:main.php");
+    die();
 
 ?>
