1230bb234edef0ae7869cb72136e482a5048f968
Keaton Sant <san16072@byui.edu>
2019-10-24 14:43:33 -0600

    move index

--- web/index.php
+++ web/index.php
@@ -1,5 +1,74 @@
 <?php
-
-  phpinfo();
+#phpinfo();
+    session_start();
+    if (isset($_SESSION['login'])) {
+        $_SESSION['login'] = $_SESSION['login'];
+    } else {
+        $_SESSION['login'] = $_GET["fname"];
+    }
+    
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="shark.png" type="image/gif">
+    <link rel="stylesheet" type="text/css" href="homepage.css">
+    <title>Epic Sharks | Welcome</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class = "itemL">
+                <a href="shark.png">
+                    <img id="shark" src="shark.png" alt="Image of a cartoon shark">
+                </a>
+            </div>
+            <div class = "itemR">
+                <h1>Welcome <?php echo $_SESSION['login']; ?></h1>
+            </div>
+        </div>
+    </header>
+    <div class="navbar sticky">
+        <a class="link" href="aboutme.php">About Me</a>
+        <a class="link" href="assignments.php">Assignments</a>        
+    </div>
+    <div class = "container2">
+        <div class = "item_left">
+            <img id="me" src="me.jpg" alt="A professional photograph of Keaton Sant">
+        </div>
+        <div class = "item_right intro">
+            <div class = "purpose">
+                 <h2>Purpose</h2>
+            </div>
+            <hr>
+            <div>
+                <p>
+                 Welcome to my home page! My name is Keaton Sant
+                 and the purpose of this website is to introduce 
+                 myself and to show the projects I will be working 
+                 on over a period of 13 weeks!
+                </p>
+                <form action="homepage.php" method="get">
+                    <input type="text" placeholder="First Name" name="fname">
+                    <input type="submit" value="Log In">
+                </form>
+            </div>
+        </div>
+    </div>
+    
+    <div>
+        <footer>
+            <p>
+                This page was designed by Keaton Sant<br>
+                The Shark Icon was designed by Keaton Sant
+            </p>
+            <p><b>&copy; 2019 Keaton Sant<b></p>
+        </footer>
+    </div>
+</body>
+</html>
