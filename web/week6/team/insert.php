63876fa307a0cce19bd912035847ac53d46bda5f
Keaton Sant <san16072@byui.edu>
2019-10-24 00:41:14 -0600

    add to database

--- web/week6/team/insert.php
+++ web/week6/team/insert.php
@@ -87,7 +87,8 @@ if (isset($book)) {
           $stmt->bindValue(':scr_id', $row['id'], PDO::PARAM_INT);
           $stmt->execute();
           $toppics = $stmt->fetchAll(PDO::FETCH_ASSOC);
-          if ($toppics != "") {
+          vardump($toppics);
+          if (isset($toppics)) {
               if (sizeof($toppics) > 1) {
                 echo " Topics: ";
               } else {echo " Topic: ";}
