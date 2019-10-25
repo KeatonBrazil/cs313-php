4125fde9b35c8bd49ec72e4885f64ab82e40e42b
Keaton Sant <san16072@byui.edu>
2019-10-25 09:53:36 -0600

    js

--- web/games/game.js
+++ web/games/game.js
@@ -1,1 +1,8 @@
-
+function hide() {
+    var x = document.getElementById("dude");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
--- web/games/main.php
+++ web/games/main.php
@@ -73,24 +73,5 @@
             ?>
         </div>
     </div>
-<script>
-function hide() {
-    var x = document.getElementById("dude");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-
-
-
-//   var x = document.getElementByClassName("myLink");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
-}
-</script>
 </body>
 </html>
