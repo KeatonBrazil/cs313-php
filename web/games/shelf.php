1ff0041e14d3b0aa9d32e3138bdcdc61110385c1
Keaton Sant <san16072@byui.edu>
2019-10-26 16:08:36 -0600

    display game shelf

--- web/games/shelf.php
+++ web/games/shelf.php
@@ -67,7 +67,7 @@ $game_info = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 $stmt->bindValue(':g_id', $game_info[$i]['bg.game_id'], PDO::PARAM_INT);
                 $stmt->execute();
                 $pub = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                var_dump($pub);
+                var_dump($game_info[$i]['bg.game_id']);
 
                 echo "<div class='post'>";
                 echo $game_info[$i]['title'].'<br><hr>';
