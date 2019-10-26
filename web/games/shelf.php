d2307346eabc61f342cac7a7217422f0ba440510
Keaton Sant <san16072@byui.edu>
2019-10-25 18:18:35 -0600

    new nav bar for shelf

--- web/games/shelf.php
+++ web/games/shelf.php
@@ -36,22 +36,13 @@ else
         <?php if ($username === 'Admin') {echo "<li class='mylink'><a href='admin.php'>Admin</a></li>";} ?>
         <li class="floatright mylink"><a href="signOut.php">Sign Out</a></li>
         <li class="floatright user">Welcome <?php echo $username; ?></li>
-        </div>
+        </ul>
+        <ul class="nav">
+        <li class="mylink"><a href="newGame.php">Request New Game</a></li>
+        <li class="active mylink"><a href="addGame.php">Add Game to Shelf</a></li>
         </ul>
     </div>    
-    <div class="sticky">
-        <ul class="nav">
-        <li class="menu" onclick="hide()">Menu</li>
-        <div id="dude">
-        <li class="mylink"><a href="main.php">Hub</a></li>
-        <li class="active mylink"><a href="shelf.php">Game Shelf</a></li>
-        <li class="mylink"><a href="wish.php">Wish List</a></li>
-        <li class="mylink"><a href="friends.php">Friends</a></li>
-        <?php if ($username === 'Admin') {echo "<li class='mylink'><a href='admin.php'>Admin</a></li>";} ?>
-        <li class="floatright mylink"><a href="signOut.php">Sign Out</a></li>
-        <li class="floatright user">Welcome <?php echo $username; ?></li>
-        </div>
-        </ul>
-    </div>  
+    
+       
 </body>
 </html>
