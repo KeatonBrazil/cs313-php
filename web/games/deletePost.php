2d9b5651263dc62cb919d903ea1d069e304845a6
Keaton Sant <san16072@byui.edu>
2019-10-27 13:07:20 -0600

    delete post

--- web/games/deletePost.php
+++ web/games/deletePost.php
@@ -15,14 +15,12 @@
     $db = get_db();
 
     $post_id = $_POST['post_id'];
-    $locate = $post_id - 1;
-    echo $post_id;
 
     $query = 'DELETE FROM game.post WHERE post_id=:post_id';
     $stmt = $db->prepare($query);
     $stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
     $stmt->execute();
 
-    // header("Location: main.php#post$locate");
-    // die();
+    header("Location: main.php");
+    die();
 ?>
