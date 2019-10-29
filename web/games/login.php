3c7db6edc0a5a8fe55c7cc1d43f0d0469e101a3a
Keaton Sant <san16072@byui.edu>
2019-10-29 00:59:52 -0600

    sign up restraints

--- web/games/login.php
+++ web/games/login.php
@@ -66,7 +66,7 @@ if (isset($_POST['user']) && isset($_POST['pass']))
         <form action='login.php' method='post'>
         <input type="text"  name="user" placeholder="Username"/><br><br>
         <input type="password"  name="pass" placeholder="Password"/><br>
-        <p><?php if ($badLogin) {echo 'Your username or password is incorrect';} ?></p>
+        <p><?php if ($badLogin) {echo "<span class='red'>Your username or password is incorrect</span>";} ?></p>
         <input class="submit" type="submit" value="Login">
         </form>
     </div>
