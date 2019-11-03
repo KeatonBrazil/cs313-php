3fe04e9cb252ac8a0481d8b795d59bd2a6af0d8f
Keaton Sant <san16072@byui.edu>
2019-11-02 22:59:53 -0600

    add game to shelf

--- web/games/addGame.php
+++ web/games/addGame.php
@@ -20,12 +20,14 @@ $p_id0 = $_POST['p_id0'];
 var_dump($g_id);
 var_dump($p_id0);
 
-// $query = 'SELECT member_id FROM game.member WHERE username = :user';
-// $stmt = $db->prepare($query);
-// $stmt->bindValue(':user', $username, PDO::PARAM_STR);
-// $stmt->execute();
-// $mem_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// $mem_id = $mem_id[0]['member_id'];
+$query = 'SELECT member_id FROM game.member WHERE username = :user';
+$stmt = $db->prepare($query);
+$stmt->bindValue(':user', $username, PDO::PARAM_STR);
+$stmt->execute();
+$mem_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$mem_id = $mem_id[0]['member_id'];
+
+var_dump($mem_id);
 
 // $query = 'SELECT shelf_id FROM game.gameShelf WHERE member_id = :mem_id';
 // $stmt = $db->prepare($query);
