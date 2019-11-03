26fe13a424e8626ecb01ed8f4cd3ad93857276b1
Keaton Sant <san16072@byui.edu>
2019-11-02 23:14:22 -0600

    add game to shelf

--- web/games/addGame.php
+++ web/games/addGame.php
@@ -17,17 +17,12 @@ $db = get_db();
 $g_id = $_POST['newGame'];
 $p_id0 = $_POST['p_id0'];
 
-var_dump($g_id);
-var_dump($p_id0);
-
 $query = 'SELECT member_id FROM game.member WHERE username = :user';
 $stmt = $db->prepare($query);
 $stmt->bindValue(':user', $username, PDO::PARAM_STR);
 $stmt->execute();
 $mem_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $mem_id = $mem_id[0]['member_id'];
-
-var_dump($mem_id);
 
 $query = 'SELECT shelf_id FROM game.gameShelf WHERE member_id = :mem_id';
 $stmt = $db->prepare($query);
@@ -36,8 +31,6 @@ $stmt->execute();
 $shelf_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $shelf_id = $shelf_id[0]['shelf_id'];
 
-var_dump($shelf_id);
-
 $query = 'SELECT boardGame_id FROM game.boardGame WHERE game_id = :game_id AND publisher_id = :pub_id';
 $stmt = $db->prepare($query);
 $stmt->bindValue(':game_id', $g_id, PDO::PARAM_INT);
@@ -45,8 +38,6 @@ $stmt->bindValue(':pub_id', $p_id0, PDO::PARAM_INT);
 $stmt->execute();
 $bg_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $bg_id = $bg_id[0]['boardgame_id'];
-
-var_dump($bg_id);
 
 $query = 'INSERT INTO game.collection (shelf_id, boardGame_id) VALUES (:shelf, :bg_id)';
 $stmt = $db->prepare($query);
