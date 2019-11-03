bcad22a108c47538ee0b617b9e702db913f8a970
Keaton Sant <san16072@byui.edu>
2019-11-02 22:53:51 -0600

    add game to shelf

--- web/games/addGame.php
+++ web/games/addGame.php
@@ -58,7 +58,7 @@ $stmt->bindValue(':shelf_id', $shelf_id, PDO::PARAM_INT);
 $stmt->bindValue(':bg_id', $bg_id, PDO::PARAM_INT);
 $result = $stmt->execute();
 
-header("location: viewGames.php#return");
+header("location: shelf.php#return");
 die();
 
 ?>
